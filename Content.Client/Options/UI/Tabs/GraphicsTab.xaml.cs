eac998256f734009e60060a825a4d53d1179a80b
godot04 <168072650+Godot04@users.noreply.github.com>
2026-03-04 13:08:37 +0100

    replaced EP to ECHO

--- Content.Client/Options/UI/Tabs/GraphicsTab.xaml.cs
+++ Content.Client/Options/UI/Tabs/GraphicsTab.xaml.cs
@@ -70,9 +70,9 @@ public sealed partial class GraphicsTab : Control
         Control.AddOptionCheckBox(CCVars.ViewportScaleRender, ViewportLowResCheckBox, invert: true);
         Control.AddOptionCheckBox(CCVars.ParallaxLowQuality, ParallaxLowQualityCheckBox);
         Control.AddOptionCheckBox(CCVars.HudFpsCounterVisible, FpsCounterCheckBox);
-        // PE-Tweak-start
+        // ECHO-Tweak-start
         Control.AddOptionCheckBox(EchoCCVars.FilmGrain, FilmGrainCheckBox);
-        // PE-Tweak-end
+        // ECHO-Tweak-end
 
         Control.Initialize();
 
